a7d9bbacca9a723efb4fc73fe30f73346507e9b8
Charlie Harris <47694815+cch1999@users.noreply.github.com>
2023-04-15 12:34:17 +0100

    Add init alaises

---
+++ aliases.sh
@@ -0,0 +1,18 @@
+
+
+# Conda
+alias ca="conda activate"
+
+
+# Mamba
+alias mm="micromamba"
+alias mma="mm activate"
+
+# Directories
+alias sbdd="cd ~/projects/ligand-pocket-ddpm"
+
+
+# For CSD3
+alias shared=""
+alias work=""
+
